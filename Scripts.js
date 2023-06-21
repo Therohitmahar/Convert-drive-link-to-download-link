fc1a02a0b33973a4414c75545e4907dc15eb43d2
rohit mahar <therohitmahar@gmail.com>
2023-06-21 22:46:10 +0530

    Bootsrap and navbar

--- Scripts.js
+++ Scripts.js
@@ -14,17 +14,23 @@ convertBtn.onclick = async (e) => {
     e.preventDefault();
 
     main = `https://drive.google.com/u/0/uc?id=${id}&export=download`;
-    resultContainer.innerHTML = `<h3 id="result">${main}</h3>
-    <button id="copy">Copy</button>`
+    resultContainer.innerHTML = `<h3 class="text-light" id="result">${main}</h3>
+    <button class="btn btn-success" id="copy">Copy</button>`
 
 
     document.getElementById("copy").onclick = async () => {
         try {
-
+            document.getElementById("toast").innerText = "Copied to Clipboard"
             await navigator.clipboard.writeText(main);
+            setTimeout(() => {
+                document.getElementById("toast").innerText = ""
+            }, 1000);
         } catch (err) {
             console.log(err);
         }
     };
+
+
 };
 console.log(main);
+
